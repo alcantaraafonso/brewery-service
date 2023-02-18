70f07a4727827d2a552f342de81d103e5fc3c2eb
Afonso Alcantara <alcantara.afonso@gmail.com>
2023-02-18 16:06:33 -0300

    Adding Beer Respository

---
+++ src/main/java/br/com/beganinha/breweryservice/repository/BeerRepository.java
@@ -0,0 +1,11 @@
+package br.com.beganinha.breweryservice.repository;
+
+import br.com.beganinha.breweryservice.domain.Beer;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> {
+}
