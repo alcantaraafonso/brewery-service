9c5d366ff2a5f43a76b3a4ea120d8979f9053b0c
Afonso Alcantara <alcantara.afonso@gmail.com>
2023-02-20 19:54:33 -0300

    Adding Java Bean Validation to BeerDto

--- src/main/java/br/com/beganinha/breweryservice/web/model/BeerDto.java
+++ src/main/java/br/com/beganinha/breweryservice/web/model/BeerDto.java
@@ -1,5 +1,9 @@
 package br.com.beganinha.breweryservice.web.model;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Null;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,19 +19,31 @@ import java.util.UUID;
 @Builder
 public class BeerDto {
 
+    @Null
     private UUID id;
 
+    @Null
     private Integer version;
 
+    @Null
     private OffsetDateTime createdDate; //supports UTC
 
+    @Null
     private OffsetDateTime lastModifedDate;
 
+    @NotBlank
     private String beerName;
 
+    @NotNull
     private BeerStyleEnum beerStyle;
 
+    @NotNull
+    @Positive
     private Long upc;
 
+    @NotNull
+    @Positive
     private BigDecimal price;
+
+    private Integer quantityOnHand;
 }
