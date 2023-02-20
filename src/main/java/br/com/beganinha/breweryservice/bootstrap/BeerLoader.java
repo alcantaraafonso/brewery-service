537f2eb42755d2f21bf708b53b5d34d697c698db
Afonso Alcantara <alcantara.afonso@gmail.com>
2023-02-20 19:47:42 -0300

    Adding Beer Respository and Entity annotations

---
+++ src/main/java/br/com/beganinha/breweryservice/bootstrap/BeerLoader.java
@@ -0,0 +1,53 @@
+package br.com.beganinha.breweryservice.bootstrap;
+
+import br.com.beganinha.breweryservice.domain.Beer;
+import br.com.beganinha.breweryservice.repository.BeerRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+
+@Component
+@Slf4j
+/**
+ * This class is used to load data into the H2 Database
+ */
+public class BeerLoader implements CommandLineRunner {
+
+    private final BeerRepository repository;
+
+    public BeerLoader(BeerRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        loadBeerObjects();
+    }
+
+    private void loadBeerObjects() {
+        if(repository.count() == 0) {
+            repository.save(Beer.builder()
+                    .beerName("Mango Bobs")
+                    .beerStyle("IPA")
+                    .quantityToBrew(200)
+                    .minOnHand(12)
+                    .upc(337010000001L)
+                    .price(new BigDecimal("12.95"))
+                    .build());
+
+            repository.save(Beer.builder()
+                    .beerName("Galaxy Cat")
+                    .beerStyle("PALE_ALE")
+                    .quantityToBrew(200)
+                    .minOnHand(12)
+                    .upc(337010000002L)
+                    .price(new BigDecimal("11.95"))
+                    .build());
+        }
+
+        log.info("Loaded beers: " + repository.count());
+        repository.findAll().forEach(b -> log.info("Beers' IDs: " + b.getId()));
+    }
+}
--- src/main/java/br/com/beganinha/breweryservice/repository/BeerRepository.java
+++ src/main/java/br/com/beganinha/breweryservice/repository/BeerRepository.java
@@ -1,11 +1,12 @@
 package br.com.beganinha.breweryservice.repository;
 
 import br.com.beganinha.breweryservice.domain.Beer;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 @Repository
-public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> {
+public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID>, CrudRepository<Beer, UUID> {
 }
