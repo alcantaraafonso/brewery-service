44226b3343b1305c4e412a36296c68a51ed215ea
Afonso Alcantara <alcantara.afonso@gmail.com>
2023-02-24 21:11:33 -0300

    Adding Jackson annotations

--- src/main/java/br/com/beganinha/breweryservice/web/model/BeerDto.java
+++ src/main/java/br/com/beganinha/breweryservice/web/model/BeerDto.java
@@ -1,5 +1,6 @@
 package br.com.beganinha.breweryservice.web.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Null;
@@ -25,9 +26,11 @@ public class BeerDto {
     @Null
     private Integer version;
 
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ssZ", shape = JsonFormat.Shape.STRING)
     @Null
     private OffsetDateTime createdDate; //supports UTC
 
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ssZ", shape = JsonFormat.Shape.STRING)
     @Null
     private OffsetDateTime lastModifedDate;
 
@@ -41,6 +44,7 @@ public class BeerDto {
     @Positive
     private Long upc;
 
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
     @NotNull
     @Positive
     private BigDecimal price;
